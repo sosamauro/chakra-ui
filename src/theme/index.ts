6205595360806a1be00eacb86d3256ad51115f85
Mauro Sosa <mauro4sosa@gmail.com>
2022-03-28 15:45:58 -0300

    update

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -55,7 +55,7 @@ const theme = extendTheme(
     components: {
       Button: {
         variants: {
-          primary: (props) => ({
+          primary: (props: any) => ({
             rounded: 'none',
             ...brandRing,
             color: mode('white', 'gray.800')(props),
